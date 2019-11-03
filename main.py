b99107cda9a97759ef1f15c44414e3ffb4e72ac8
Akanksha Kumari <akanksha@Akankshas-MacBook-Pro.local>
2019-11-03 13:21:38 -0500

    Update mfcc features of all files

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import feature_mfcc
-
+import os
+import numpy as np
 
 if __name__ == "__main__":
-    
-    path = "speech-accent-archive/recordings/recordings/indonesian1.mp3"
-    mfcc = feature_mfcc.getMFCC(path)
-    print (mfcc)
+    filelist = os.listdir("speech-accent-archive/recordings/recordings")
+    path_1 = "speech-accent-archive/recordings/recordings/"+filelist[0]
+    mfcc_array = feature_mfcc.getMFCC(path_1)
+    filelist.pop(0)
+    for i in filelist:
+        path = "speech-accent-archive/recordings/recordings/"+i
+        mfcc = feature_mfcc.getMFCC(path)
+        np.append(mfcc_array,mfcc)
+np.save('outfilemfcc', mfcc_array)
