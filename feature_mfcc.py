c28d7b9072e5eb57071cd42f3294c0ad65381059
Akanksha Kumari <akanksha@lawn-128-61-70-135.lawn.gatech.edu>
2019-11-11 15:21:14 -0500

    added waveform and mfcc images

--- feature_mfcc.py
+++ feature_mfcc.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 def getMFCC(path):
     waveform, sampling_rate = librosa.load(path)
     mfcc = librosa.feature.mfcc(waveform,sampling_rate,n_mfcc=20)
+    #plt.figure(figsize=(10, 4))
+    #librosa.display.specshow(mfcc, x_axis='time')
+    plt.figure()
+    librosa.display.waveplot(waveform, sr=sampling_rate)
+    plt.title('Waveform')
+    plt.show()
     return mfcc
 
 
