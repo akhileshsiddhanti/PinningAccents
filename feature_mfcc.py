bc6da5b9347aa1c96b76ea70f9ba4e92cc638f8a
KathanKP <kathan.kashiparekh@gatech.edu>
2019-11-14 23:59:13 -0500

    Some changes

--- feature_mfcc.py
+++ feature_mfcc.py
@@ -10,21 +10,31 @@ AUDIO_PATH = "speech-accent-archive/recordings/recordings/"
 def get_features(path):
     accent = re.split('\d',path)[0]
     print(accent)
-    waveform, sampling_rate = librosa.load(AUDIO_PATH + path)
-    stft = np.abs(librosa.stft(waveform))
-    mfcc = np.mean(librosa.feature.mfcc(waveform,sampling_rate,n_mfcc=20), axis=1)
-    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sampling_rate), axis=1)
-    mel = np.mean(librosa.feature.melspectrogram(waveform, sr=sampling_rate), axis=1)
-    spec_bw = np.mean(librosa.feature.spectral_bandwidth(y=waveform, sr=sampling_rate), axis=1)
-    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sampling_rate), axis=1)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(waveform), sr=sampling_rate), axis=1)
+    waveform, sampling_rate = librosa.load(AUDIO_PATH + path, duration=15, sr=22050)
+    mfcc = librosa.feature.mfcc(waveform,sampling_rate,n_mfcc=20,hop_length=1024)
+    delta = librosa.feature.delta(mfcc)
+    features = np.hstack((mfcc.flatten(), delta.flatten()))
 
-    features = np.hstack([mfcc, chroma, mel, spec_bw, contrast, tonnetz])
+    # MFCC Image
+    plt.figure()
+    librosa.display.specshow(mfcc, x_axis='time')
+    plt.savefig('Images/'+path.split('.')[0])
+    plt.close()
+    
     return features, accent
+    # stft = np.abs(librosa.stft(waveform))
+    # chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sampling_rate), axis=1)
+    # mel = np.mean(librosa.feature.melspectrogram(waveform, sr=sampling_rate), axis=1)
+    # spec_bw = np.mean(librosa.feature.spectral_bandwidth(y=waveform, sr=sampling_rate), axis=1)
+    # contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sampling_rate), axis=1)
+    # tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(waveform), sr=sampling_rate), axis=1)
+
+    # features = np.hstack([mfcc, chroma, mel, spec_bw, contrast, tonnetz])
+    # return features, accent
 
 
 if __name__ == '__main__':
 
-    path = "speech-accent-archive/recordings/recordings/english1.mp3"
-    others = get_features(path)
+    path = "english1.mp3"
+    get_features(path)
 
--- main.py
+++ main.py
@@ -27,5 +27,6 @@ if __name__ == "__main__":
     unique_accents, mapped_values = np.unique(all_accents, return_inverse=True)
     mapped_values = np.expand_dims(mapped_values, axis=1)
     final_dataset = np.hstack((all_features, mapped_values))
-    np.save('final_datasettop3.npy', final_dataset)
-    np.save('unique_accentstop3.npy', unique_accents)
+    np.save('mfcc_delta_data.npy', final_dataset)
+    np.save('unique_accents_mfcc_delta.npy', unique_accents)
+    np.save('filelist_order.npy', filelist)
